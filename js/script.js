06fa3a3acff5b38456dc6831e022d77f260a9a1c
pinomartin <martin91pino@gmail.com>
2019-12-16 21:11:18 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ let enlaces = document.getElementById("enlaces");
 
 let contador = 0;
 
-boton.addEventListener("click", function() {
+boton.addEventListener("click", function(e) {
+    e.preventDefault();
     if (contador == 0) {
         enlaces.className = ("enlaces dos");
         contador = 1;

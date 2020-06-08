d02a27b2be3e913490e0209d49778760d18cf361
pinomartinUTN <110933@tecnicatura.frc.utn.edu.ar>
2020-06-07 22:10:05 -0300

    delete wave effect

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 let boton = document.getElementById("icono");
 let enlaces = document.getElementById("enlaces");
+let btnSkills = document.getElementById("skill");
+let btnXP = document.getElementById("xp");
+let btnContacto = document.getElementById("contacto");
 
 let contador = 0;
 
-boton.addEventListener("click", function(e) {
-    e.preventDefault();
+boton.addEventListener("click", accionMenuDefault );
+btnSkills.addEventListener("click", accionOpcionesMenu );
+btnXP.addEventListener("click", accionOpcionesMenu );
+btnContacto.addEventListener("click", accionOpcionesMenu );
+
+
+
+
+function accionMenuDefault(e) {
+   e.preventDefault();
     if (contador == 0) {
         enlaces.className = ("enlaces dos");
         contador = 1;
@@ -13,8 +24,23 @@ boton.addEventListener("click", function(e) {
         enlaces.className = ("enlaces uno");
         contador = 0;
     }
-});
+}
 
+
+function accionOpcionesMenu() {
+    
+     if (contador == 0) {
+         enlaces.className = ("enlaces dos");
+         contador = 1;
+     } else {
+         enlaces.classList.remove("dos");
+         enlaces.className = ("enlaces uno");
+         contador = 0;
+     }
+ }
+
+
+/**Ir a Contacto */
 $(document).ready(function(){
     'use strict';
     $("#boton-contacto").click(function() {
@@ -23,6 +49,12 @@ $(document).ready(function(){
                     }, 2000);
             }
     );
+    $("#btn-home").click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#divskills-h1").offset().top
+                }, 1000);
+        }
+);
 });
 
 
